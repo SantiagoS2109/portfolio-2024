ba9a1a769b8731662c5d6ee7636423377190f274
SantiagoS2109 <santiagosepul2109@gmail.com>
2024-02-19 15:18:11 -0500

    Background Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,8 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div> */}
-      <div>
+      <div className="relative">
+        <div className="absolute bottom-0 top-0 z-[-2] min-h-screen w-full bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
         <Nav isIntersecting={isIntersecting} />
         <main className="mx-auto max-w-4xl  px-4 py-8">
           <SectionHero reference={ref} />
